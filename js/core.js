824e46079544cfa62cafd31cc839d440adf0d0e7
liang wang <iamhtie@gmail.com>
2011-02-22 01:00:54 +0800

    much smart url analyser,and more smooth image show up

--- js/core.js
+++ js/core.js
@@ -25,6 +25,8 @@ var IC = {
 	//
 	image_broken:true,
 	index_pointer:-1,
+	last_position:-1,
+	number_rollen:[],
 	last_image:null,
 	url_segs:[],
 	//
@@ -32,51 +34,56 @@ var IC = {
 		this.url_segs.length = 0;
 		//http://fs2ew.redirectme.net/girl/67/01.jpg
 		var segs = url.split(/(\/){1}/);
-		var length = segs.length;
-		for(var i=0;i<length-1;i++){
-			
-			if(isNaN(this.toInt(segs[i]))){
-				this.url_segs.push(segs[i]);
-			}else{
-				this.url_segs.push(this.toInt(segs[i]));
-			}
-		}
+		this.url_segs = segs;
 		//image
-		var image_name = segs[length-1].split(/(\.){1}/);
-		for(var j=0;j<image_name.length;j++){
-			if(isNaN(this.toInt(image_name[j]))){
-				this.url_segs.push(image_name[j]);
-			}else{
-				this.url_segs.push(this.toInt(image_name[j]));
+		var length = segs.length;
+		var image_name = segs[length-1].split(/(\.){1}/); 
+		this.url_segs[length-1] = image_name[0];
+		this.url_segs.push(image_name[1]);
+		this.url_segs.push(image_name[2]);
+		
+		for(var i=0;i<length;i++){
+			if(/(\d)+?$/.test(segs[i])){
+				this.number_rollen[i] = true;
 			}
 		}
+		this.index_pointer = this.number_rollen.length-1;
+		this.last_position = this.index_pointer;
 		
 	},
 	show_url:function(div){
 		var frag = document.createDocumentFragment();
 		for(var i=0;i<this.url_segs.length;i++){
 			var control ;
-			if(typeof this.url_segs[i] == "string"){
-				control = document.createElement("label");
-				control.innerHTML = this.url_segs[i];
-			}else{
+			if(this.number_rollen[i]){
 				control = document.createElement("input");
 				control.value = this.url_segs[i];
+			}else{
+				control = document.createElement("label");
+				control.innerHTML = this.url_segs[i];
 			}
 			frag.appendChild(control);
 		}
 		this.id(div).appendChild(frag);
 	},
 	locate_index_pointer:function(){
-		var last_position;
-		if(this.index_pointer == -1){
-			last_position = this.url_segs.length;
-		}else{
-			last_position = this.index_pointer;
+		for(var i=this.index_pointer;i>=this.last_position;i--){
+			var pad = this.url_segs[i].length;
+			this.url_segs[i] = "";
+			while(pad){
+				this.url_segs[i] +="0";
+				pad--;
+			}
 		}
-		for(var i=last_position-1;i>=0;i--){
-			if(!isNaN(this.toInt(this.url_segs[i]))){
-				this.index_pointer = i;
+		
+		for(var i=this.last_position-1;i>=0;i--){
+			if(this.number_rollen[i]){
+				
+				var walker = this.toInt(this.url_segs[i]);
+				walker++;
+				this.url_segs[i] = walker;
+				
+				this.last_position = i;
 				break;
 			}
 		}
@@ -90,28 +97,37 @@ var IC = {
 		var walker =  this.url_segs[this.index_pointer];
 		var walker2 = this.toInt(walker);
 		walker2++;
-		if(walker2<10){
-			walker = "0"+ walker2;	
-		}else{
-			walker = walker2;
+		
+		var pad_length = walker.length-walker2.toString().length;
+		while(pad_length){
+			walker2 = "0"+ walker2;
+			pad_length--;
 		}
+		walker = walker2;
+	
 		this.url_segs[this.index_pointer] = walker;
 		
 		/*This is wired.If don't do this assignment,
-		 * container in function of justify_gallery is undefined.
+		 * container in function of image.onload is undefined.
 		 */
 		var container = container;
 		var monitor = monitor;
-		var image = new Image();
-		var justify_gallery = function(){
-			var width = this.toInt(this.id(container).offsetWidth)+image.width+5;//5 fix gap
-			this.id(container).style.width = width +"px";
-		}.bind(this,image);
 		//
+		var image = new Image();
 		image.className = "sample-image";
 		image.onload =  function(){
 			this.id(monitor).innerHTML = "Image has loaded ";
-			justify_gallery();
+			//
+			var width = this.toInt(this.id(container).offsetWidth)+image.width+5;//5 fix gap
+			this.id(container).style.width = width +"px";
+			//
+			if(this.last_image == null){
+				this.id(container).appendChild(image);
+			}else{
+				this.id(container).insertBefore(image,this.last_image);
+			}
+			//
+			this.last_image = image;
 			//
 			this.image_broken = false;
 		}.bind(this);
@@ -125,13 +141,7 @@ var IC = {
 		
 		image.src = this.url_segs.join("");
 		this.id(monitor).innerHTML = "Image is loading ,please wait ";
-		if(this.last_image == null){
-			this.id(container).appendChild(image);
-		}else{
-			this.id(container).insertBefore(image,this.last_image);
-		}
-		//
-		this.last_image = image;
+		
 	}
 }
 
